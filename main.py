5e6645a37ab9b9f2803f075628efbf0a7495462c
MisamoXD <80806278+MisamoXD@users.noreply.github.com>
2021-03-18 00:54:57 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ Builder.load_string("""
     background_color: 0,0,0,0  
     color: 1,1,0,1
     size: 150,150
-    font_size: 20
+    font_size: 60
     text_size: self.size
     halign: 'center'
     valign: 'bottom'
@@ -29,95 +29,51 @@ Builder.load_string("""
         Rectangle:
             pos: self.pos
             size: self.size
-            source: 'plantimage.jpg'
     FloatLayout:
         RoundSquare:
             text: 'Auto Detect'
             on_release: root.manager.current = 'detect'
-            size_hint: None, None
-            pos: root.width /2-self.width/2-115, root.top/2+80
+            size_hint: 0.35,0.35/2
+            pos_hint: {'top':0.80,'right':0.45}
             Image:
-                source: 'cameraicon.png'
-                pos: root.width /2-self.width/2-115, root.top/2+105
-                size: 110,110
+            	source: 'cameraicon.png'
+            	size: 300,290
+            	x: self.parent.x+self.parent.width-330
+            	y: self.parent.y+self.parent.height-300
+            	allow_stretch: True
         RoundSquare:
             text: 'Manual Input'
             on_release: root.manager.current = 'manual'
-            size_hint: None,None
-            pos: root.width /2-self.width/2+130, root.top/2+80
+            size_hint: 0.35,0.35/2
+            pos_hint: {'top':0.80,'right':0.90}
             Image:
                 source: 'paperwrite.png'
-                pos: root.width /2-self.width/2+140, root.top/2+115
-                size: 100,100
+            	size: 300,290
+            	x: self.parent.x+self.parent.width-325
+            	y: self.parent.y+self.parent.height-300
+            	allow_stretch: True
         RoundSquare:
             text: 'Help'
             on_release: root.manager.current = 'help'
-            size_hint: None,None
-            pos: root.width /2-self.width/2-115, root.top/2-180
+            size_hint: 0.35,0.35/2
+            pos_hint: {'top':0.40,'right':0.45}
             Image:
                 source: 'questionmark.png'
-                pos: root.width /2-self.width/2-115, root.top/2-145
-                size: 100,100
+            	size: 300,290
+            	x: self.parent.x+self.parent.width-330
+            	y: self.parent.y+self.parent.height-300
+            	allow_stretch: True
         RoundSquare:
             text: 'Exit'
             on_release: app.stop()
-            size_hint: None,None
-            pos: root.width /2-self.width/2+130, root.top/2-180
+            size_hint: 0.35,0.35/2
+            pos_hint: {'top':0.40,'right':0.90}
             Image:
                 source: 'exitimage.png'
-                pos: root.width /2-self.width/2+130, root.top/2-145
-                size: 100,100
- 
- 
-<DetectPage>:
-    BoxLayout:
-        orientation: 'vertical'
-        Camera:
-            id: camera
-            resolution: (640, 480)
-            play: False
-        ToggleButton:
-            text: 'Play'
-            on_release: camera.play = not camera.play
-            size_hint_y: None
-            height: '48dp'
-        Button:
-            text: 'Capture'
-            size_hint_y: None
-            height: '48dp'
-            on_press: root.capture()
-        Button:
-            text: 'Main Page'
-            size_hint: 1, None
-            height: '48dp'
-            on_release: root.manager.current = 'main'
- 
- 
-<ManualPage>:
-    id: imageviewer
-    BoxLayout:
-        orientation: 'vertical'
-        Image: 
-            id: my_image
-            source: ""
- 
-        FileChooserIconView:
-            id: imagechooser
-            on_selection: imageviewer.selected(imagechooser.selection)
-        Button:
-            text: 'Main Page'
-            size_hint: 1, None
-            on_release: root.manager.current = 'main'
- 
-<HelpPage>:
-    BoxLayout:
-        orientation: 'vertical'
-        Label:
-            text: 'Help Page'
-        Button:
-            text: 'Main Page'
-            size_hint: 1, None
-            on_release: root.manager.current = 'main'
+            	size: 300,290
+            	x: self.parent.x+self.parent.width-330
+            	y: self.parent.y+self.parent.height-300
+            	allow_stretch: True
 """)
  
 # Declare screens
@@ -126,35 +82,11 @@ Builder.load_string("""
 class MainPage(Screen):
     pass
  
- 
-class DetectPage(Screen):
-    def capture(self):
-        camera = self.ids['camera']
-        timestr = time.strftime("%Y%m%d_%H%M%S")
-        camera.export_to_png("IMG_" + timestr)
-        print("Captured")
- 
- 
-class ManualPage(Screen):
-    def selected(self, filename):
-        try:
-            self.ids.my_image.source = filename[0]
-        except:
-            pass
- 
- 
-class HelpPage(Screen):
-    pass
- 
- 
 class MainApp(App):
     def build(self):
         # Create the screen manager
         sm = ScreenManager()
         sm.add_widget(MainPage(name='main'))
-        sm.add_widget(DetectPage(name='detect'))
-        sm.add_widget(ManualPage(name='manual'))
-        sm.add_widget(HelpPage(name='help'))
         return sm
  
  
